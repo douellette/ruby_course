597c7d911fd84c43b7838b8a362116537e07ec82
Dana Ouellette <dana.ouellette@concordia.ab.ca>
2013-11-26 08:54:36 -0700

    Nov 26

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,5 @@ class Review < ActiveRecord::Base
   attr_accessible :article, :date, :poster, :title
   validates :title, :presence => true
   validates :poster, :presence => true
+  has_many :comments
 end
